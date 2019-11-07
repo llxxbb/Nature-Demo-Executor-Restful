8c38f9127ba5be98dba5b7242c889a2a58ba44fa
李学斌 <lixuebin@cdeledu.com>
2019-11-07 09:19:28 +0800

    add framework for rest converter

--- src/bin/restful_converter.rs
+++ src/bin/restful_converter.rs
@@ -1,9 +1,26 @@
 extern crate dotenv;
 
+use std::env;
+
+use actix_web::{App, HttpResponse, HttpServer, web};
+use actix_web::web::Json;
 use dotenv::dotenv;
+
+use nature_common::ConverterParameter;
+
+fn index(_para: Json<ConverterParameter>) -> HttpResponse {
+    HttpResponse::Ok().json("x")
+}
+
 
 fn main() {
     dotenv().ok();
+    let port = env::var("DEMO_CONVERTER_PORT").unwrap_or_else(|_| "8082".to_string());
+    HttpServer::new(
+        || App::new().service(
+            web::resource("/{id}/{name}/index.html").to(index)))
+        .bind("127.0.0.1:".to_owned() + &port).unwrap()
+        .run().unwrap();
 }
 
 
